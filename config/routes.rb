ad2dc87ba224bcea5d10abcfbd623b78bf6e94ef
Vladislav <dreamervt8@gmail.com>
2025-04-22 12:58:29 +0500

    chore: hide rswag from production

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => "/api-docs"
-  mount Rswag::Api::Engine => "/api-docs"
+  if Rails.env.development? || Rails.env.test?
+    mount Rswag::Ui::Engine => "/api-docs"
+    mount Rswag::Api::Engine => "/api-docs"
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
   get "status", to: "health#status"
 

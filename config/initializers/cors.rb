ced8d9d8b55eae023821b5a95a20df5c55fba8a1
Vladislav <dreamervt8@gmail.com>
2025-04-27 01:02:15 +0500

    chore: small fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "https://fastyshop.kz"
+    origins "http://localhost:7777", "https://fastyshop.kz"
 
     resource "*",
       headers: :any,
